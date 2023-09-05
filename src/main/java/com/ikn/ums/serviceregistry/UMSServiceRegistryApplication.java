6ea0edd3377435d2661fac31e6d6d31cc30ed2f1
Vaishnav Puthiyanamparambath <vaishnav.p@ikcontech.com>
2023-09-05 15:24:58 +0530

    added comment

--- src/main/java/com/ikn/ums/serviceregistry/UMSServiceRegistryApplication.java
+++ src/main/java/com/ikn/ums/serviceregistry/UMSServiceRegistryApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+//
 public class UMSServiceRegistryApplication {
 
 	public static void main(String[] args) {
